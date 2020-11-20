f45c81a675c4f4cb4acc7bd3b842335fb0a7255a
Philipp Borgers <borgers@mi.fu-berlin.de>
2020-11-20 15:38:37 +0100

    improve test script

--- test_rand.sh
+++ test_rand.sh
@@ -1,4 +1,6 @@
 INTERFACE="wlan0"
+DATA_DIR="/tmp/searchwing-wifibroadcast-test"
+WFB_DIR=""
 BREAK=1
 WARM_UP=2
 WORK_TIME=4
@@ -6,44 +8,40 @@ CHANNEL=11
 
 mode=${2}
 
+mkdir -p ${DATA_DIR}
+
 while IFS=, read -r testid FEC_d FEC_r mcs Txpower stbc ldpc bandwidth
 do
-    start=$(($(date +%s%N) / 1000000))
     read up rest </proc/uptime; start="${up%.*}${up#*.}"
-    
+
     echo "Starting test testid: ${testid} fec_d: ${FEC_d} fec_r: ${FEC_r} mcs: ${mcs} txpower: ${Txpower} stbc: ${stbc} ldpc: ${ldpc} bandwidth: ${bandwidth}"
-    
+
     #setup interface
     iw dev ${INTERFACE} set channel ${CHANNEL} ${bandwidth}MHz &
-    
+
     if [ ${mode} == "tx" ]; then
+      echo "starting transmitter"
       # wait for the rx to start listening
       sleep $((${BREAK} + ${WARM_UP}))
       # Spawn a child process:
-      (cat /dev/zero | tx -r ${FEC_r} -b ${FEC_d} ${INTERFACE}) &
-      pid=$!
-      echo ${pid}
-      sleep ${WORK_TIME} 
-      #kill -9 ${pid}
+      (cat /dev/zero | ${WFB_DIR}/tx -r ${FEC_r} -b ${FEC_d} ${INTERFACE}) &
+      sleep ${WORK_TIME}
       killall tx
       # buffer for rx
       sleep $((${BREAK} + ${WARM_UP}))
     else
       echo "starting receiver"
       sleep ${BREAK}
-      #TODO add date to file name
-      (./rx -r ${FEC_r} -b ${FEC_d} ${INTERFACE} > "/tmp/searchwing-${testid}.data") & pid=$!
-      (tcpdump -i ${INTERFACE} -w "/tmp/searchwing-tcpdump-${testid}.pcap") & tcpdump_pid=$!
-      #TODO save debug script output
+      (tcpdump -i ${INTERFACE} -w "${DATA_DIR}/searchwing-tcpdump-${testid}.pcap") & tcpdump_pid=$!
+      (${WFB_DIR}/rx -r ${FEC_r} -b ${FEC_d} ${INTERFACE} > "${DATA_DIR}/searchwing-${testid}.data") & pid=$!
+      (${WFB_DIR}/rx_status_csv -f "${DATA_DIR}/searchwing-debug-${testid}.csv") &
       sleep $((${WORK_TIME} + ${WARM_UP} + ${WARM_UP}))
-      #(./rx_status_test > /tmp/searchwing-debug-${testid}.txt) &
-      kill -9 $pid
-      kill -9 $tcpdump_pid
+      killall rx
+      killall tcpdump
+      killall rx_status_csv
       sleep ${BREAK}
     fi
-    end=$(($(date +%s%N)/1000000))
     read up rest </proc/uptime; end="${up%.*}${up#*.}"
-    echo ${end} ${start}
     runtime=$((10*(${end}-${start})))
     echo "Runtime: ${runtime}"
 
