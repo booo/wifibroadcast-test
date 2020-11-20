dd7f07fe7407c16a31d9d7981f774154385ddbb4
petrosilius <petrosilius@posteo.de>
2020-11-20 14:26:12 +0100

    changed csv read for new setup

--- test_rand.sh
+++ test_rand.sh
@@ -2,19 +2,25 @@ INTERFACE="wlan0"
 BREAK=1
 WARM_UP=2
 WORK_TIME=4
+CHANNEL=11
 
 mode=${2}
 
-while IFS=, read -r testid fec_d fec_r bitrate txpower
+while IFS=, read -r testid FEC_d FEC_r mcs Txpower stbc ldpc bandwidth
 do
     start=$(($(date +%s%N) / 1000000))
     read up rest </proc/uptime; start="${up%.*}${up#*.}"
-    echo "Starting test testid: ${testid} fec_d: ${fec_d} fec_r: ${fec_r} bitrate: ${bitrate} txpower: ${txpower}"
+    
+    echo "Starting test testid: ${testid} fec_d: ${FEC_d} fec_r: ${FEC_r} mcs: ${mcs} txpower: ${Txpower} stbc: ${stbc} ldpc: ${ldpc} bandwidth: ${bandwidth}"
+    
+    #setup interface
+    iw dev ${INTERFACE} set channel ${CHANNEL} ${bandwidth}MHz &
+    
     if [ ${mode} == "tx" ]; then
       # wait for the rx to start listening
       sleep $((${BREAK} + ${WARM_UP}))
       # Spawn a child process:
-      (cat /dev/zero | tx -r ${fec_r} -b ${fec_d} ${INTERFACE}) &
+      (cat /dev/zero | tx -r ${FEC_r} -b ${FEC_d} ${INTERFACE}) &
       pid=$!
       echo ${pid}
       sleep ${WORK_TIME} 
@@ -26,7 +32,7 @@ do
       echo "starting receiver"
       sleep ${BREAK}
       #TODO add date to file name
-      (./rx -r ${fec_r} -b ${fed_d} ${INTERFACE} > "/tmp/searchwing-${testid}.data") & pid=$!
+      (./rx -r ${FEC_r} -b ${FEC_d} ${INTERFACE} > "/tmp/searchwing-${testid}.data") & pid=$!
       (tcpdump -i ${INTERFACE} -w "/tmp/searchwing-tcpdump-${testid}.pcap") & tcpdump_pid=$!
       #TODO save debug script output
       sleep $((${WORK_TIME} + ${WARM_UP} + ${WARM_UP}))
