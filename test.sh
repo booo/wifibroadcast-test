38460d8d4b3b0c59a9afe35e9f10911e10b2c3ae
Philipp Borgers <borgers@mi.fu-berlin.de>
2020-12-04 12:43:31 +0100

    save ieee80211 debug dir before and after test run

--- test.sh
+++ test.sh
@@ -24,7 +24,7 @@ do
     do
        iw dev ${if} set monitor otherbss fcsfail &
        iw dev ${if} set channel ${channel} ${bandwidth} &
-    done    
+    done
 
     #setup interface
     if [ ${mode} == "tx" ]; then
@@ -45,10 +45,11 @@ do
       do
          (tcpdump -i ${if} -w "${DATA_DIR}/searchwing-tcpdump-mon0-${testid}.pcap") & tcpdump_pid=$!
       done
-      
+      (mkdir -p "${DATA_DIR}/${testid}-ieee80211" && cp -r /sys/kernel/debug/ieee80211 "${DATA_DIR}/${testid}-ieee80211/start") &
       (${WFB_DIR}/rx -r ${FEC_r} -b ${FEC_d} ${INTERFACE} > "${DATA_DIR}/searchwing-${testid}.data") & pid=$!
       (${WFB_DIR}/rx_status_csv -f "${DATA_DIR}/searchwing-debug-${testid}.csv") &
       sleep $((${WORK_TIME} + ${WARM_UP} + ${WARM_UP}))
+      (cp -r /sys/kernel/debug/ieee80211 "${DATA_DIR}/${testid}-ieee80211/end") &
       killall rx &
       killall tcpdump &
       killall rx_status_csv &
