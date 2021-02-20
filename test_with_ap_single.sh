26056169143e3747193eb30aab077edab11a7fb6
petrosilius <petrosilius@posteo.de>
2021-02-20 16:14:10 +0100

    added testids

---
+++ test_with_ap_single.sh
@@ -0,0 +1,22 @@
+
+mode=${1}
+
+SLEEPTIME=20
+read up rest </proc/uptime; start="${up%.*}${up#*.}"
+
+if [ ${mode} == "tx" ]; then
+        uci set wireless.ap.disabled=1 && wifi && sleep 5 &&
+	iw dev mon0 set channel 2 5Mhz && iw dev mon0 set channel 3 5Mhz &  # fucking shit
+fi
+sleep ${SLEEPTIME}
+
+read up rest </proc/uptime; end="${up%.*}${up#*.}"
+runtime=$((10*(${end}-${start})))
+echo "AP testmode waittime: ${runtime}"
+
+./test.sh testids_single.csv ${mode}
+
+if [ ${mode} == "tx" ]; then
+        uci set wireless.ap.disabled=0 && uci commit && wifi &
+fi
+
